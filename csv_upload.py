9593229af6a5dfcec21e63dd9ee5f2db4ca27a08
uiooaua <haruna.sugimoto@iterra.co.jp>
2024-05-30 09:25:15 +0900

    second commit

--- csv_upload.py
+++ csv_upload.py
@@ -8,11 +8,33 @@ credentials = service_account.Credentials.from_service_account_file(
     'banana-ojt-d74de788e054.json', # サービスアカウントキー名の指定
     scopes=['https://www.googleapis.com/auth/devstorage.read_write'],
 )
-storage_client = storage.Client(
-    credentials = credentials,
-    project = credentials.project_id,
-)
+'''storage_client = storage.Client(
+        credentials = credentials,
+        project = credentials.project_id,
+    )
+'''
+def upload_blob_from_memory(bucket_name, contents, destination_blob_name):
 
+    storage_client = storage.Client(
+        credentials = credentials,
+        project = credentials.project_id,
+    )
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_string(contents)
+
+    print(
+        f"{destination_blob_name} with contents {contents} uploaded to {bucket_name}."
+    )
+
+if __name__ == '__main__':
+    bucket_name = 'test-iterra'
+    contents = 'test-cat.csv'
+    destination_blob_name = 'nyanko.jpg'
+
+'''    
 def change_suffix(file_name, from_suffix, to_suffix):
     #ファイルの拡張子を得る
     sf = pathlib.PurePath(file_name).suffix
@@ -29,9 +51,4 @@ def change_suffix(file_name, from_suffix, to_suffix):
         shutil.move(file_name, to_name)
 
 if __name__ == '__main__':
-    change_suffix('test-cat.csv', '.csv', '.jpg')
-
-# バケット内へのファイルのアップロード
-'''bucket = storage_client.get_bucket('test-iterra') # バケット名の指定
-blob = bucket.blob('test-cat-csv') # バケット内のファイル名の指定
-blob.upload_from_filename('test-cat.csv') # ローカルファイル名の指定'''
+    change_suffix('./image/test-cat.jpg', '.jpg', '.csv')'''
